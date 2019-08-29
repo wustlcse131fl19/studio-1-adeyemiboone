c84fb5185079fec60e82a6e8ce851388cc65487a
kiranboone <kiranboone@wustl.edu>
2019-08-29 15:10:16 -0500

    HiFour and Ordered done!

--- src/studio1/HiFour.java
+++ src/studio1/HiFour.java
@@ -9,10 +9,12 @@ import support.cse131.ArgsProcessor;
 public class HiFour {
 	public static void main(String[] args) {
 		ArgsProcessor ap = new ArgsProcessor(args);
-		String s0 = ap.nextString("Enter value for arg 0");
-		String s1 = ap.nextString("Enter value for arg 1");
-		String s2 = ap.nextString("Enter value for arg 2");
-		String s3 = ap.nextString("Enter value for arg 3");
+		String name1 = ap.nextString("What's your name, my green companion?");
+		String name2 = ap.nextString("And you, strange man?");
+		String name3 = ap.nextString("And what do they call you?");
+		String name4 = ap.nextString("Comment t'appelle tu?");
+		System.out.println ("What's poppin', " + name1 + ", " + name2 + ", " + name3 + ", and " + name4 + "! " +
+				"PREPARE FOR YOUR GRAND ADVENTURE!");
 		//
 		// Say hello to the names in s0 through s3.
 		//
